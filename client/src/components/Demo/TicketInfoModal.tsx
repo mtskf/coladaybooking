f38bff08d2ce42ce7e9e29ddb13615f6738adbb2
Mitsuki Fukunaga <mitsuki.fukunaga@spiritsuper.com.au>
2022-06-26 21:50:10 +1000

    refactor

--- client/src/components/Demo/TicketInfoModal.tsx
+++ client/src/components/Demo/TicketInfoModal.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { VpnKey, Lock, LockOpen, NoEncryption } from '@mui/icons-material';
+import { VpnKey, Lock, LockOpen, NoEncryption, DeleteForever } from '@mui/icons-material';
 import { Ticket } from 'types';
 import styles from "./styles.module.scss";
 
@@ -58,8 +58,8 @@ function TicketInfoModal ({ isActive, ticket, remove, close, decryptTicketTitle 
         <small>{ticket.date}&nbsp;&nbsp;{ticket.from}:00 to {ticket.to}:00&nbsp;&nbsp;&nbsp;&nbsp;Room {ticket.room}</small>
 
         <div className="buttons">
-          <button className="button is-simple is-rounded" onClick={() => remove(ticket)}>Delete event</button>
-          <button className="button is-primary is-rounded" onClick={close}>Close</button>
+          <button className="button is-simple is-rounded" onClick={() => remove(ticket)}><DeleteForever />Delete</button>
+          <button className="button is-primary is-rounded" onClick={close}>OK</button>
         </div>
 
       </div>
