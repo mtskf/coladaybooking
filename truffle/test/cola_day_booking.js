5bfac3e2021d227c3a4e35cd93023c4aa89bc1a7
Mitsuki Fukunaga <mitsuki.fukunaga@spiritsuper.com.au>
2022-06-27 15:10:05 +1000

    refactor - test contract

--- truffle/test/cola_day_booking.js
+++ truffle/test/cola_day_booking.js
@@ -1,11 +1,14 @@
+// const truffleAssert = require('truffle-assertions')
+
 const ColaDayBooking = artifacts.require("ColaDayBooking")
+const rooms = ["C01", "C02", "C03", "C04", "C05", "C06", "C07", "C08", "C09", "C10", "P01", "P02", "P03", "P04", "P05", "P06", "P07", "P08", "P09", "P10"]
 
 contract("ColaDayBooking", (accounts) => {
   let instance
   beforeEach(async () => {
     instance = await ColaDayBooking.new()
   })
-  xcontext("Booking", async () => {
+  context("Booking", async () => {
     it("Book a room1", async () => {
       try {
         await instance.book("test", "C01", 8, 2, false)
@@ -88,7 +91,7 @@ contract("ColaDayBooking", (accounts) => {
     })
     it("Wrong room name should fail", async () => {
       try {
-        await instance.book("test", "D01", 8, 2)
+        await instance.book("test", "D01", 8, 2, false)
       } catch (err) {
         const errorMessage = "Room not found"
         assert.equal(err.reason, errorMessage, "Error as expected")
@@ -97,20 +100,21 @@ contract("ColaDayBooking", (accounts) => {
       assert(false, "should fail")
     })
   })
-  context("Load slots data", async () => {
+  context("Get slots data", async () => {
     it("Load 20 rooms", async () => {
-      const rooms = await instance.getSlots()
-      assert.equal(rooms.length, 20, "20 rooms loaded")
-      assert.equal(rooms[0].length, 10, "10 slots in C01")
-      assert.equal(rooms[1].length, 10, "10 slots in C02")
-      assert.equal(rooms[10].length, 10, "10 slots in P01")
-      assert.equal(rooms[19].length, 10, "10 slots in P10")
+      const slots = await instance.getSlots()
+      assert.equal(slots.length, 20, "20 rooms loaded")
+      assert.equal(slots[0].length, 10, "10 slots in C01")
+      assert.equal(slots[1].length, 10, "10 slots in C02")
+      assert.equal(slots[10].length, 10, "10 slots in P01")
+      assert.equal(slots[19].length, 10, "10 slots in P10")
     })
   })
-  context("Load tickets data", async () => {
+  context("Get tickets data", async () => {
     it("Booking a room1", async () => {
       try {
         await instance.book("test", "C01", 8, 2, false)
+        await instance.book("encrypted text dummy", "P05", 13, 4, true)
       } catch (err) {
         assert.equal(false, "Cannot book", err.reason)
         return
@@ -124,7 +128,91 @@ contract("ColaDayBooking", (accounts) => {
       assert.equal(tickets[0].from, "8", "from ok")
       assert.equal(tickets[0].duration, "2", "duration ok")
       assert.equal(tickets[0].isActive, true, "isActive ok")
+      assert.equal(tickets[0].isEncrypted, false, "isEncrypted ok")
+
+      assert.equal(tickets[1].id, 1, "ticket id ok")
+      assert.equal(tickets[1].title, "encrypted text dummy", "title ok")
+      assert.equal(tickets[1].room, "P05", "room ok")
+      assert.equal(tickets[1].from, "13", "from ok")
+      assert.equal(tickets[1].duration, "4", "duration ok")
+      assert.equal(tickets[1].isActive, true, "isActive ok")
+      assert.equal(tickets[1].isEncrypted, true, "isEncrypted ok")
     })
   })
+  context("Remove ticket", async () => {
+    it("Booking rooms", async () => {
+      try {
+        await instance.book("test", "C01", 8, 2, false)
+        await instance.book("test2", "P05", 13, 4, false)
+        await instance.book("test3", "C10", 12, 1, false)
+      } catch (err) {
+        assert.equal(false, "Cannot book", err.reason)
+        return
+      }
+      const tickets = await instance.getTickets()
+      const slots = await instance.getSlots()
+
+      assert(true, "Got tickets")
+      assert.equal(tickets[0].id, 0, "ticket id ok")
+      assert.equal(tickets[1].title, "test2")
+      assert.equal(tickets[2].from, 12)
+
+      assert(true, "Got slots -> check availability")
+      const test1RoomIndex = rooms.indexOf(tickets[0].room)
+      slots[test1RoomIndex][0] = false
+      slots[test1RoomIndex][1] = false
+      slots[test1RoomIndex][2] = true
+
+      const test2RoomIndex = rooms.indexOf(tickets[1].room)
+      slots[test2RoomIndex][4] = true
+      slots[test2RoomIndex][5] = false
+      slots[test2RoomIndex][6] = false
+      slots[test2RoomIndex][7] = false
+      slots[test2RoomIndex][8] = false
+      slots[test2RoomIndex][9] = true
+
+      const test3RoomIndex = rooms.indexOf(tickets[2].room)
+      slots[test2RoomIndex][3] = true
+      slots[test2RoomIndex][4] = false
+      slots[test2RoomIndex][5] = true
+
+
+    })
+    it("Booking rooms", async () => {
+      try {
+        await instance.book("test", "C01", 8, 2, false)
+        await instance.book("test2", "P05", 13, 4, false)
+        await instance.book("test3", "C10", 12, 1, false)
+      } catch (err) {
+        assert.equal(false, "Cannot book", err.reason)
+        return
+      }
+      let tickets = await instance.getTickets()
+
+      assert(true, "Remove ticket")
+      await instance.removeTicket(tickets[0].id)
+      tickets = await instance.getTickets()
+      const slots = await instance.getSlots()
+
+      const test1RoomIndex = rooms.indexOf(tickets[0].room)
+      slots[test1RoomIndex][0] = true
+      slots[test1RoomIndex][1] = true
+      slots[test1RoomIndex][2] = true
+
+    })
+  })
+  // context("Event", async () => {
+  //   it("Booking rooms", async () => {
+  //     // truffleAssert.eventEmitted(result, 'Updated', (ev) => {
+  //     //   console.log(ev)
+  //     //   return true
+  //     // })
+  //     assertEventOfType: function(response, eventName, index) {
+  //       assert.equal(response.logs[index].event, eventName, eventName + ' event should fire.')
+  //     }
+  //     await instance.book("test", "C01", 8, 2, false)
+
+  //   })
+  // })
 
 })
