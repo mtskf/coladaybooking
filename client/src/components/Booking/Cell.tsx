5a5dc5dd8ac22b3a48c5a8d6e171d850f16a8e87
Mitsuki Fukunaga <mitsuki.fukunaga@spiritsuper.com.au>
2022-06-30 00:59:10 +1000

    refactor

--- client/src/components/Booking/Cell.tsx
+++ client/src/components/Booking/Cell.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useRef } from "react";
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 import { Ticket } from "types";
-import TicketItem from "./TicketItem"
+import TicketItem from "./TicketItem";
 
 interface PropsType {
   key: number;
@@ -22,21 +22,21 @@ interface PropsType {
 function Cell ({ ...props }: PropsType) {
 
   const nextProps = useMemo<PropsType>(() => {
-    return { ...props }
+    return { ...props };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.selecting, props.selected, props.disabled, props.decrypted, props.booked, props.making, props.deleting])
+  }, [props.selecting, props.selected, props.disabled, props.decrypted, props.booked, props.making, props.deleting]);
 
   const tdRef = useRef<HTMLTableCellElement>(null);
 
   const handleTouchStart = (e: any) => {
     if (!nextProps.disabled) {
-      nextProps.onTouchStart(e)
+      nextProps.onTouchStart(e);
     }
   };
 
   const handleTouchMove = (e: any) => {
     if (!nextProps.disabled) {
-      nextProps.onTouchMove(e)
+      nextProps.onTouchMove(e);
     }
   };
 
@@ -58,11 +58,11 @@ function Cell ({ ...props }: PropsType) {
   const classNames = useMemo(() => {
     const classNames = []
     if (props.ticket) {
-      classNames.push(styles.hasTicket)
+      classNames.push(styles.hasTicket);
     }
 
     if (props.booked) {
-      classNames.push(styles.booked)
+      classNames.push(styles.booked);
     } else if (props.making) {
       classNames.push(styles.making);
     } else if (props.making) {
@@ -70,20 +70,19 @@ function Cell ({ ...props }: PropsType) {
     }
 
     if (props.disabled) {
-      classNames.push(styles.disabled)
+      classNames.push(styles.disabled);
     } else {
-      classNames.push(styles.enabled)
+      classNames.push(styles.enabled);
       if (props.selected) {
-        classNames.push(styles.selected)
-      }
-      if (props.selecting) {
-        classNames.push(styles.selecting)
+        classNames.push(styles.selected);
+      } else if (props.selecting) {
+        classNames.push(styles.selecting);
       }
     }
 
     return classNames;
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.selecting, props.selected, props.disabled, props.decrypted, props.booked, props.making, props.deleting])
+  }, [props.selecting, props.selected, props.disabled, props.decrypted, props.booked, props.making, props.deleting]);
 
   return (
     <td
